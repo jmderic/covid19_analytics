ca79975875cada0aacda04a15cb8f28c8c04fd10
Mark Deric <codesmith@dericnet.com>
2020-04-05 13:16:14 -0700

    Add plot creation

--- test_prog.py
+++ test_prog.py
@@ -13,8 +13,11 @@ from argparse import ArgumentParser
 
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 
-def test_func(csv_file, death_delay, recover_delay):
+#from matplotlib.ticker import ScalarFormatter
+
+def compute_active(csv_file, death_delay, recover_delay):
     pd.set_option('display.width', 120)
     # pd.set_option('display.max_colwidth', 10) # does not seem to work
     type_spec = { 'Confirmed' : np.int32, 'Deaths' : np.int32 }
@@ -23,7 +26,6 @@ def test_func(csv_file, death_delay, recover_delay):
     #df1['Date'] = df1['Date'].astype(Timestamp)
     #df1['Deaths'] = df1['Deaths'].astype(int)
     #df1['Confirmed'] = df1['Confirmed'].astype(int)
-    #df1 = df2.drop(['Cum C', 'Cum D'], axis=1)
     date_list = df1['Date'].tolist()
     #print(date_list)
     df1['DieLater'] = 0
@@ -52,8 +54,6 @@ def test_func(csv_file, death_delay, recover_delay):
     df2['RecLater'] = df2['Confirmed'] - df2['DieLater']
     #df2['CumDieLater'] = df2['DieLater'].cumsum()
     #df2['CumConf'] = df2.groupby('Date')['Confirmed'].transform(pd.Series.cumsum)
-    #df2['PotRec'] = df2['CumConf'] - df2['CumDieLater']
-    #print(df2['Date'].iat[-1])
     end_date = df2['Date'].iat[-1]
     #print(df2)
 
@@ -86,14 +86,42 @@ def test_func(csv_file, death_delay, recover_delay):
                 df2 = df2.append(add_row_dict, ignore_index=True)
 
     df3 = df2.sort_values(by=['Date'])
-    df3 = df3[['Date', 'Confirmed', 'Deaths', 'DieLater', 'RecLater', 'Recovered']]
-    df3 = df3.reset_index(drop=True)
+    df3 = df3.set_index('Date')
+    #df3 = df3[['Date', 'Confirmed', 'Deaths', 'DieLater', 'RecLater', 'Recovered']]
+    df3 = df3[['Confirmed', 'Deaths', 'DieLater', 'RecLater', 'Recovered']]
+    #df3 = df3.reset_index(drop=True)
     df3['Active'] = df3['Confirmed'] - df3['Deaths'] - df3['Recovered']
     df3['CumConf'] = df3['Confirmed'].cumsum()
     df3['CumDeaths'] = df3['Deaths'].cumsum()
     df3['CumRec'] = df3['Recovered'].cumsum()
     df3['CumActive'] = df3['Active'].cumsum()
-    print(repr(df3))
+    print(df3)
+    return df3
+
+def plot_active(df1, Log10):
+    # dataframe fixup
+    unwanted_cols = ['Confirmed', 'Deaths', 'DieLater', 'RecLater',
+                     'Recovered', 'Active']
+    df1 = df1.drop(unwanted_cols, axis=1)
+    if Log10:
+        unwanted = df1[(df1['CumConf']==0) | (df1['CumDeaths']==0)
+                       | (df1['CumRec']==0) | (df1['CumActive']==0)]
+    else:
+        unwanted = df1[(df1['CumConf']<=10) & (df1['CumDeaths']<=10)
+                       & (df1['CumRec']<=10) & (df1['CumActive']<=10)]
+    df1 = df1.drop(unwanted.index)
+    print(df1)
+    # do the plotting
+    #plt.figure()
+    df1.plot()
+    if Log10:
+        plt.yscale('log')
+        #plt.ticklabel_format(axis='y', style='plain')
+        #fig, ax = plt.subplots()
+        #ax.yaxis.set_major_formatter(ScalarFormatter())
+    plt.legend(loc='best', labels=['Confirmed', 'Deaths', 'Recovered', 'Active'])
+    plt.savefig('plot.svg')
+    #plt.show()
 
 class FindActive:
     def __init__(self, cl_args):
@@ -101,9 +129,11 @@ class FindActive:
         self.csv_file = cl_args.csv_file
         self.death_delay = cl_args.death_delay
         self.recover_delay = cl_args.recover_delay
+        self.log_plot = cl_args.log_plot
 
     def run(self):
-        test_func(self.csv_file, self.death_delay, self.recover_delay)
+        df = compute_active(self.csv_file, self.death_delay, self.recover_delay)
+        plot_active(df, self.log_plot)
 
 def call_runner(cl_args) :
     os.umask(0o002)
@@ -120,6 +150,7 @@ if __name__ == '__main__':
     p_start_idea.add_argument('-d', '--death-delay', help='Avg. days, symptomatic to death', type=int, default=17)
     p_start_idea.add_argument('-r', '--recover-delay', help='Avg. days, symptomatic to recovery', type=int, default=14)
     p_start_idea.add_argument('-i', '--csv-file', help='CSV input file', type=str, required=True)
+    p_start_idea.add_argument('-l', '--log-plot', help='Plot y axis as log', action='store_true')
 
     # generic subcommand execution call
     parsed_args = parser.parse_args()
