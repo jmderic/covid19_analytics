21248150b8899100288597280583ca0cc6d219dd
Mark Deric <codesmith@dericnet.com>
2020-06-27 16:36:17 -0700

    Save working API Orange County filter; very messy

--- api_test.py
+++ api_test.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-import urllib.request, json, datetime
+import urllib.request, urllib.parse, json, datetime
 
 
 def get_ts_fm_dt(dt) :
@@ -11,17 +11,23 @@ def get_timestamp() :
     return get_ts_fm_dt(dt_now)
 
 #url = 'https://data.ca.gov/api/3/action/datastore_search?resource_id=926fd08f-cc91-4828-af38-bd45de97f8c3'
-#url = 'https://data.ca.gov/api/3/action/datastore_search?resource_id=926fd08f-cc91-4828-af38-bd45de97f8c3&limit=5'
-url = 'https://data.ca.gov/api/3/action/help_show?name=datastore_search'
-fileobj = urllib.request.urlopen(url)
+url = 'https://data.ca.gov/api/3/action/datastore_search?resource_id=926fd08f-cc91-4828-af38-bd45de97f8c3&limit=32000&filters={"county": "Orange"}'
+# filters=%7B%22county%22%3A%20%22Orange%22%7D
+safe_url = 'https://data.ca.gov/api/3/action/datastore_search?resource_id=926fd08f-cc91-4828-af38-bd45de97f8c3&limit=32000&filters=%7B%22county%22%3A%20%22Orange%22%7D'
+#url = 'https://data.ca.gov/api/3/action/help_show?name=datastore_search'
+
+#safe_url = urllib.parse.quote(url)
+#print(safe_url)
+
+fileobj = urllib.request.urlopen(safe_url)
 #print fileobj.read()
 
 parsed = json.load(fileobj)
 
 ts = get_timestamp()
 
-with open(f'/tmp/api_test_{ts}.txt', 'w') as f:
-    f.write(parsed["result"])
+#with open(f'/tmp/api_test_{ts}.txt', 'w') as f:
+#    f.write(parsed["result"])
 
 with open(f'/tmp/api_test_{ts}.json', 'w') as f:
     json.dump(parsed, f, indent=4, sort_keys=True)
