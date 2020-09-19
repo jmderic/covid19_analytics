a45792e17248b9540cad40f7e08fc7c2cc63ec80
Mark Deric <codesmith@dericnet.com>
2020-09-19 10:16:41 -0700

    Improve the add to exec-path comment
    Offer a simple shell line to generate the elisp function.

--- emacs_fixups.el
+++ emacs_fixups.el
@@ -1,5 +1,9 @@
 (setq comint-process-echoes t)
-;;; your location may vary
+
+;;; update emacs' exec-path after you do pipenv shell
+;;; run the result of this shell command in the *scratch* buffer
+;;; echo "(add-to-list 'exec-path \"$(pipenv --venv)/bin\")"
+;;; e.g.,
 (add-to-list 'exec-path "/home/mark/.local/share/virtualenvs/covid19_analytics-Jk6VWX-K/bin")
 
 ;; -*- indent-tabs-mode: nil -*-
